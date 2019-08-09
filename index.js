dbbcf26566b38f8ba8fa46aa5a37fce31e59f7d6
Paulina <aniskiewicz.paulina@gmail.com>
2019-08-09 16:47:10 +0200

    4 exercises added

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 //1.
-const obj = { first: Jane, last: Doe };
+const obj = {
+  first: "Jane",
+  last: "Doe"
+};
+
+const { first, last } = obj;
+console.log(first, last); //Jane, Doe
+
+//2. Destructure and rename
+const { first: f, last: l } = obj;
+console.log(f, l); //Jane, Doe
+
+//3. Default value
+const { foo = 3, bar } = {};
+console.log(foo, bar); // 3, undefined
+console.log("");
+
+//4. Default value and rename
+const { foo: x = 3, bar: y } = {};
+console.log(x, y); //3, undefined
